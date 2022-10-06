8eaf4c141fd938bf5d166d49da7c6b2ab59ad5f4
Collin Adele <adele.collin@student-cs.fr>
2022-10-06 21:06:25 +0200

    Fixed basic model

--- MVP1.py
+++ MVP1.py
@@ -4,25 +4,23 @@ from scipy import integrate as sp
 import matplotlib.pyplot as plt
 
 # General carateristics
-time_span = 30
-time_step = 3
+time_span = 100
+time_points = 300
 
 # Constantes definition TO BE CHANGED FOLLOWING BIBLIOGRAPHY
 # Intermediates
 k_cat = 2.68 #[µM/s]
 Km = 45.0 #[µM]
-E0 = 10.0
+#E0 = 10.0
 
 # Mediator reduction
 k_2 = 0.722 #[1/s]
 
 # Mediator oxidation
-n = 2.0
-F = 96500
 k_3 = 0.15  #[1/s]
+k_4 = 28.95 #[mA]
 
 # ODE function
-
 
 def ODE(x, t):
     # Stoichiometric matrix
@@ -37,10 +35,10 @@ def ODE(x, t):
 
     # Rate matrix
     RM = np.matrix([
-                   [k_cat*E0*(x[1]/(Km+x[1]))],  # intermediates raye
+                   [k_cat*(x[1]/(Km+x[1]))],  # intermediates raye
                    [k_2*x[0]*x[2]],  # mediator reduction rate
                    [k_3*x[3]],  # mediator oxidation rate
-                   [n*F*k_3*x[3]], # current conversion from new electronic input
+                   [k_4*x[3]], # current conversion from new electronic input
                    [x[4]] # already existing electic flux
                    ])
 
@@ -50,13 +48,13 @@ def ODE(x, t):
 
 # ODE solution using scipy package
 # Time distribution to simulate
-t = np.linspace(0, time_span, time_step)
+t = np.linspace(0, time_span, time_points)
 
 # Initial conditions (state variable values at t=0)
 NADH_0 = 0.0
-NAD_0 = 100.0
+NAD_0 = 50.0
 MB_ox_0 = 0.0
-MB_red_0 = 100.0
+MB_red_0 = 50.0
 I_0 = 0.0
 
 x0 = [NADH_0, NAD_0, MB_ox_0, MB_red_0, I_0]
@@ -67,7 +65,7 @@ plt.plot(t, out[:, 0], label = '[NADH] (M)')
 plt.plot(t, out[:, 1], 'tab:green', label ='[NAD](M)')
 plt.plot(t, out[:, 2], 'tab:orange', label = '[MB_ox](M)')
 plt.plot(t, out[:, 3], 'tab:pink', label = '[MB_red] (M)')
-#plt.plot(t, out[:, 4], 'tab:red', label = 'I (A)')
+plt.plot(t, out[:, 4], 'tab:red', label = 'I (A)')
 plt.xlabel('Time (s)')
 plt.legend()
 # fig, axs = plt.subplots(5, 1)
